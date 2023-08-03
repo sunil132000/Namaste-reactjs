801d7787d612b199982f3a72d20888a890bf9c8d
SomeName <Some@gmail.com>
2023-08-03 16:05:22 +0530

    first commit

---
+++ Inception/with_cdn_react.js
@@ -0,0 +1,3 @@
+let first = React.createElement('h1', {}, "hello world");
+let root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(first);
---
+++ Inception/with_javascript.js
@@ -0,0 +1,5 @@
+let first = document.createElement('h1');
+first.innerHTML = "hello everyone";
+
+let second = document.getElementById('root');
+second.appendChild(first);
